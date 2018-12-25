4e5f3338902fc482efd07de0b3829e67655f0108
ltt <leitingting08@gmail.com>
2018-12-25 23:13:10 +0800

    路由配置

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,31 @@ import Login from './pages/login.jsx'
 import ErrPage from './pages/errpage.jsx'
 
 class App extends React.Component {
-  // constructor(props){
-  //   super(props)
-  // }
+  constructor(props){
+    super(props)
+  }
 
   render() {
-    // let layoutRouter = (
-    //   <Layout>
-    //     <Switch>
-    //       <Route exact path='/' component={Home}></Route>
-    //       <Route  path='/Home' component={Home}></Route>
-    //       <Route  path='/Login' component={Login}></Route>
-    //       <Route  component={ErrPage}></Route>
-    //     </Switch>
-    //   </Layout>
-    //   )
+    let layoutRouter = (
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route  path='/Home' component={Home}></Route>
+          <Route  path='/Login' component={Login}></Route>
+          <Route  component={ErrPage}></Route>
+        </Switch>
+      </Layout>
+      )
     return (
       <div>
        helloooooooooooo~
       </div>
-      // <Router>
-      //   <Switch>
-      //     <Route path='/Login' component={Login}/>
-      //     <Route path='/' render={props =>layoutRouter}/>
-      //   </Switch>
-      // </Router>
+      <Router>
+        <Switch>
+          <Route path='/Login' component={Login}/>
+          <Route path='/' render={props =>layoutRouter}/>
+        </Switch>
+      </Router>
     )
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,23 +1,20 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App.jsx';
-// import Home from './pages/home.jsx';
+// import Login from './Login.jsx';
 // // import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<Home /> , document.getElementById('app'));
 // // serviceWorker.unregister(); 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-  render(){ // Every react component has a render method.
-    return( // Every render method returns jsx. Jsx looks like HTML, but it's actually javascript and functions a lot like xml, with self closing tags requiring the `/` within the tag in order to work propperly
-      <div>
-        Hello World
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="Login">
+        login
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Login />, document.getElementById('app'));
