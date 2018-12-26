a43da95b06c07a78b4ecc635f8e9c1108d540433
ltt <leitingting08@gmail.com>
2018-12-26 20:59:55 +0800

    项目初始化配置

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App.jsx';
-// import Login from './Login.jsx';
-// // import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<Home /> , document.getElementById('app'));
-// // serviceWorker.unregister(); 
 class Login extends React.Component {
   render() {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,55 @@
-const path = require('path');
+var path = require('path')
+var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: './',
-  },
-  module: {
-    rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /(node_modules)/,
-            use: {
-              loader: 'babel-loader'
-            }
-        },
-        {
-            test: /\.(css|less)$/,
-            loader: ["style-loader", "css-loader", "less-loader"]
-        },
+    entry: path.resolve(__dirname, 'src/main.jsx'),
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js"
+    },
+    devtool:'source-map',
+    resolve:{ // 可以省略后缀名
+        extensions:['.js','.jsx']
+    },
+    module: {
+        rules: [
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less' },
+            { test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss' },
+            { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000' },  // 限制大小5kb
+            { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000'} // 限制大小小于5k
+        ]
+    },
+    plugins: [
+        // new webpack.BannerPlugin("Copyright by ltt")
+        // html 模板插件
+        new HtmlWebpackPlugin({
+            template: __dirname + '/public/index.html'
+        }),
+
+        // 热加载插件
+        new webpack.HotModuleReplacementPlugin(),
+
+        // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
+        new webpack.DefinePlugin({
+          __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+        })
     ],
-  },
-  plugins: [new HtmlWebpackPlugin()],
-  devServer: {
-        contentBase: './build', //默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录（本例设置到"build"目录）
-        historyApiFallback: true, //在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
-        inline: true, //设置为true，当源文件改变时会自动刷新页面
-        port: 8080, //设置默认监听端口，如果省略，默认为"8080"
-    }
+
+    // devServer: {
+    //     proxy: {
+    //       // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
+    //       // koa 代码在 ./mock 目录中，启动命令为 npm run mock
+    //       '/api': {
+    //         target: 'http://localhost:3000',
+    //         secure: false
+    //       }
+    //     },
+    //     contentBase: "./public", //本地服务器所加载的页面所在的目录
+    //     colors: true, //终端中输出结果为彩色
+    //     historyApiFallback: true, //不跳转--单页面应用，依赖于history API
+    //     inline: true, //实时刷新
+    //     hot: true  // 使用热加载插件 HotModuleReplacementPlugin
+    // }
 }
