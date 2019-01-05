cbd9403203b67f58147c33de38bb2f7c3f8a8634
ltt <leitingting08@gmail.com>
2019-01-05 16:53:33 +0800

    index

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import List from './subpage/list';
 
 class Header extends React.Component {
+  constructor(props, context){
+    super(props,context)
+    this.state = {
+      showul:false
+    }
+  }
   render() {
     return (
       <div className="Header">
@@ -11,9 +17,12 @@ class Header extends React.Component {
           </div>
 	        <div className="w50 txtright pr20">
           <i className="iconfont icon-xinxi mr20"></i>
-             {this.props.username}<i className="iconfont icon-arrowdropdown mr20 font30"></i>
+             <span className="username" onClick={this.clickUserName.bind(this)}>{this.props.username}
+             <i className="iconfont icon-arrowdropdown mr20 font30"></i>
+             </span>
           </div>
-          <ul className="opeartion">
+          <ul className={`opeartion ${this.state.showul?'show':'hide'}`}>
+            <span className="trangle"></span>
             <li><i className="iconfont icon-yonghu2"></i>查看档案</li>
             <li><i className="iconfont icon-xiugaimima"></i>修改密码</li>
             <li><i className="iconfont icon-tuichu"></i>退出</li>
@@ -21,6 +30,11 @@ class Header extends React.Component {
         </div>
       </div>
     )
+  }
+  clickUserName(){
+    this.setState({
+      showul:!this.state.showul
+    })
   }
 }
 
