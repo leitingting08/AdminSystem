17463542d7b8ba80c63c3f899a3f1523851d5d9d
leitingting08 <leitingting08@gmail.com>
2019-01-10 17:41:29 +0800

    mock

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -7,8 +7,8 @@ export default class Menu extends React.Component {
     super(props,context)
     this.state = {
       menus:[
-      {name:'首页',icon:'icon-shouye',show:true,link:'/home',submenu:[]},
-      {name:'公告管理',icon:'icon-gonggao',show:true,link:'/app/noticelist',submenu:[]},
+      {name:'首页',icon:'icon-shouye',show:true,link:'/home'},
+      {name:'公告管理',icon:'icon-gonggao',show:true,link:'/app/noticelist'},
       {name:'人事管理',icon:'icon-yonghu2',show:true,link:'',
       submenu:[
         {name:'员工档案',link:'/hrmanage/employeefiles',show:true},
@@ -17,18 +17,18 @@ export default class Menu extends React.Component {
         // {name:'转正申请',link:'',show:true},
         // {name:'离职申请',link:'',show:true},
       ]},
-      {name:'行政管理',icon:'icon-552cd47fba2cc',show:true,link:'',
-      submenu:[
-        // {name:'用印申请',link:'',show:true},
-        {name:'采购申请',link:'',show:true},
-        {name:'会议室预约',link:'',show:true},
-        // {name:'资料申请',link:'',show:true},
-      ]
-    },
-      {name:'组织架构',icon:'icon-zuzhi1',show:true,link:'/organization',submenu:[]},
+    //   {name:'行政管理',icon:'icon-552cd47fba2cc',show:true,link:'',
+    //   submenu:[
+    //     // {name:'用印申请',link:'',show:true},
+    //     // {name:'采购申请',link:'',show:true},
+    //     // {name:'会议室预约',link:'',show:true},
+    //     // {name:'资料申请',link:'',show:true},
+    //   ]
+    // },
+      {name:'组织架构',icon:'icon-zuzhi1',show:true,link:'/organization'},
       {name:'系统管理',icon:'icon-xitong2',show:true,link:'',
       submenu:[
-        {name:'权限设置',link:'/systemset/authorize',show:true,submenu:[]},
+        {name:'权限设置',link:'/systemset/authorize',show:true},
       ]
     },
       ]
@@ -47,7 +47,9 @@ export default class Menu extends React.Component {
               {/*<NavLink to={item.link}>*/}
               <i className={`iconfont ${item.icon}`}></i>{item.name}<i className={`youjiantou ${item.submenu?'iconfont icon-youjiantou':''}`}></i>
               {/*</NavLink>*/}
-              <SubItem items={item.submenu} />
+              {item.submenu
+              ?<SubItem items={item.submenu} />:''
+              }
 	               {/*{
                     item.submenu.map(function(subitem,subindex){
                       return <div key={subindex}>{subitem.name}</div>
@@ -64,7 +66,6 @@ export default class Menu extends React.Component {
 
   linktosonpage(item){
     console.log(this.context);
-    hashHistory.push(item.link)
     // this.context.router.push({ pathname:item.link })
     // this.props.history.push('home')
     // this.context.router.history.push('/app/noticelist');
